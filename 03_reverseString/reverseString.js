9cb79f300e34b3601bc17594cf9cabdf727ec3e0
villiamrose <villiamrose@live.com>
2022-08-21 14:26:11 +0800

    Complete initial reverseString to pass the first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const strArr = string.split('');
+  const reversedStrArr = strArr.reverse();
 
+  return reversedStrArr.join('');
 };
 
 // Do not edit below this line
