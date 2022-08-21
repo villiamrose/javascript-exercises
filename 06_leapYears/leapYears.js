56b588f28cf2ff2ea12839ce9a442145d1c5b2d2
villiamrose <villiamrose@live.com>
2022-08-21 15:59:07 +0800

    Create function to check if given year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const divisibleByFour = (year % 4) === 0;
+  const divisibleByOneHundred = (year % 100) === 0;
+  const divisibleByFourHundred = (year % 400) === 0;
 
+  const isLeapYear = (
+    (divisibleByFour || divisibleByFourHundred) && 
+    !divisibleByOneHundred
+  );
+  
+  return isLeapYear;
 };
 
 // Do not edit below this line
