8753613cbe016849aeec1739ef50a74f1f3e7576
villiamrose <villiamrose@live.com>
2022-08-21 15:13:04 +0800

    Complete initial removeFromArray to pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  const filteredArray = [];
 
+  for (item of array) {
+    if (itemsToRemove.includes(item)) continue;
+    filteredArray.push(item);
+  }
+
+  return filteredArray;
 };
 
 // Do not edit below this line
