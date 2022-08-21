07cdec2be56d2ad62de21310b4a0e1f36e0f6001
villiamrose <villiamrose@live.com>
2022-08-21 15:47:21 +0800

    Add exception handling for invalid inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function(x, y) {
+  if(
+    typeof x !== 'number' || 
+    typeof y !== 'number' ||
+    Math.min(x, y) < 0
+  ) return 'ERROR';
+
   let start = Math.min(x, y);
   let end = Math.max(x, y);
   let sum = 0;
