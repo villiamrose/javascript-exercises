bdb3e726edb0bf61a8e0b20ce2efb1ec7009a6d4
villiamrose <villiamrose@live.com>
2022-09-21 23:03:43 +0800

    Create palindromes() function, checks if the text is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(text) {
+  const cleanText = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const textArray = [...cleanText];
+  const reverseText = textArray.reverse().join('');
+  
+  return cleanText === reverseText;
 };
 
 // Do not edit below this line
