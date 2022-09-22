6b7cf15580167098df209073c43b01a68d1da49e
villiamrose <villiamrose@live.com>
2022-09-22 22:11:00 +0800

    Creat fibonacci() function, returns the nth fibonacci number

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let previous = 1;
+  let value = 0;
 
+  for (let i = 1; i <= n; i++) {
+    let currentValue = value;
+    
+    value = currentValue + previous;
+    previous = currentValue;
+  }
+
+  return value;
 };
 
 // Do not edit below this line
