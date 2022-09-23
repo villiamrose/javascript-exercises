7d8bb7da2da02874c1d2d83bd1da53c586cda9c3
villiamrose <villiamrose@live.com>
2022-09-23 22:08:13 +0800

    Create findTheOldest() function, returns oldest person from given people

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+function getAge(person) {
+  const currentYear = new Date().getFullYear();
+  const birth = person.yearOfBirth || currentYear;
+  const death = person.yearOfDeath || currentYear;
+  const age = death - birth;
 
+  return age;
+}
+
+const findTheOldest = function(people) {
+  const oldest = people.reduce(
+    (previous, current) => {
+      const previousAge = getAge(previous);
+      const currentAge = getAge(current);
+      const older = currentAge > previousAge ? current : previous;
+
+      return older;
+    }
+  );
+
+  return oldest;
 };
 
 // Do not edit below this line
